8e37299bf37af60e1c7384adc33844ff497966c0
Sergio <51530163+Sergiderakes@users.noreply.github.com>
2020-01-12 16:04:35 +0100

    Add files via upload
    Really bad collitions method

---
+++ Prueba/cuadrado_class.py
@@ -0,0 +1,77 @@
+import time
+
+class cuadrado:
+    x = 320
+    y = 240
+    x_ant = x
+    y_ant = y
+    width = 0
+    height = 0
+    vel = 0.3 # 0.3
+    is_colliding = False
+    
+    def __init__(self,x ,y, width, height, vel):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel = vel
+
+    def pos(self, x, y):
+        self.x_ant = self.x
+        self.y_ant = self.y
+        if self.x >= -50 and self.x <= self.width:
+            self.x = x
+        else:
+            self.x = self.width-10 if self.x <= -50 else -40
+
+        if self.y >= -50 and self.y <= self.height:
+            self.y = y
+        else:
+            self.y = self.height-10 if self.y <= -50 else -40
+    
+    def pos1(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def jump(self):
+        if not self.vel == 0:
+            self.y = self.y - 40
+
+    def move(self, boolean):
+        if self.is_colliding:
+            if self.x >= -50 and self.x <= self.width:
+                if boolean:
+                    self.pos1(self.x-self.vel, self.y)
+                else:
+                    self.pos1(self.x+self.vel, self.y)
+            else:
+                self.x = self.width-10 if self.x <= -50 else -40
+        else:
+            if self.x >= -50 and self.x <= self.width:
+                if boolean:
+                    self.pos(self.x-self.vel, self.y)
+                else:
+                    self.pos(self.x+self.vel, self.y)
+            else:
+                self.x = self.width-10 if self.x <= -50 else -40
+    
+    def vertical_move(self, boolean):
+        if self.is_colliding:
+            if self.y >= -50 and self.y <= self.height:
+                if boolean:
+                    self.pos1(self.x, self.y-self.vel)
+
+                else:
+                    self.pos1(self.x, self.y+self.vel)
+            else:
+                self.y = self.height-10 if self.y <= -50 else -40
+        else:
+            if self.y >= -50 and self.y <= self.height:
+                if boolean:
+                    self.pos(self.x, self.y-self.vel)
+
+                else:
+                    self.pos(self.x, self.y+self.vel)
+            else:
+                self.y = self.height-10 if self.y <= -50 else -40
--- Prueba/test_obj.py
+++ Prueba/test_obj.py
@@ -1,62 +1,74 @@
 import pygame
+import cuadrado_class as cu
 
 width, height = 640, 480
 
-class cuadrado:
-    x = 320
-    y = 240
-    vel = 0.3 # 0.3
-    
-
-    def pos(self, x, y):
-        self.x = x
-        self.y = y
-    
-    def move(self, boolean):
-        if self.x >= -50 and self.x <= width:
-            if boolean:
-                self.pos(self.x-self.vel, self.y)
-            else:
-                self.pos(self.x+self.vel, self.y)
-        else:
-            self.x = width-10 if self.x <= -50 else -40
-    
-    def vertical_move(self, boolean):
-        if self.y >= -50 and self.y <= height:
-            if boolean:
-                self.pos(self.x, self.y-self.vel)
-
-            else:
-                self.pos(self.x, self.y+self.vel)
-        else:
-            self.y = height-10 if self.y <= -50 else -40
-
-def compruebaTeclasMover(keys):
+def compruebaTeclasMover(keys, c):
     if keys[pygame.K_RIGHT]:
         c.move(False)
+    elif keys[pygame.K_d]:
+        c.move(False)
+        
     if keys[pygame.K_LEFT]:
         c.move(True)
+    elif keys[pygame.K_a]:
+        c.move(True)
+        
     if keys[pygame.K_UP]:
         c.vertical_move(True)
-    if keys[pygame.K_DOWN]:
-        c.vertical_move(False)
-    if keys[pygame.K_SPACE]:
-        c.pos(320, 240)
+    elif keys[pygame.K_w]:
+        c.vertical_move(True)
 
-cuad = pygame.image.load("imgs/25ab.png")
-cuad = pygame.transform.scale(cuad, (100, 100))
+    # if keys[pygame.K_DOWN]:
+    #     c.vertical_move(False)
+    # elif keys[pygame.K_s]:
+    #     c.vertical_move(False)
+
+    if keys[pygame.K_SPACE]:
+        print("", end = "")
+        # c.jump()
+
+def comprobar_colision(c, c1):
+    if not c == c1:
+        # print(c.x, c.y, "/", c1.x, c1.y)
+        if c.x <= c1.x + 20 and c.x + 19 > c1.x and c.y <= c1.y + 20 and c.y + 19 > c1.y:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+cuad = pygame.image.load("Prueba\\imgs\\cuad.png")
+cuad = pygame.transform.scale(cuad, (20, 20))
 
 pygame.init()
 screen = pygame.display.set_mode((width, height))
-c = cuadrado()
+c = cu.cuadrado(0, 0, width, height, 0.3)
+c1 = cu.cuadrado(320, 240, width, height, 0)
+c2 = cu.cuadrado(350, 240, width, height, 0)
+c3 = cu.cuadrado(380, 240, width, height, 0)
+c4 = cu.cuadrado(410, 240, width, height, 0)
+c5 = cu.cuadrado(440, 240, width, height, 0)
+cds = [c, c1, c2, c3, c4, c5]
 
 while True:
+    colision = False
     screen.fill(0) ##Limpia la pantalla
-    screen.blit(cuad, (c.x, c.y))
-    pygame.display.flip() ##Actualiza la pantalla
     keys = pygame.key.get_pressed()
+    for a in cds:
+        a.pos(a.x, a.y+a.vel/2)
+        screen.blit(cuad, (a.x, a.y))
+        for b in cds:
+            if not a == b:
+                colision = comprobar_colision(a, b)
+                if colision:
+                    a.is_colliding = True
+                    a.pos1(a.x_ant, a.y_ant)
+        compruebaTeclasMover(keys, a)
+    pygame.display.flip() ##Actualiza la pantalla
+    
 
-    compruebaTeclasMover(keys)
+    
     
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
